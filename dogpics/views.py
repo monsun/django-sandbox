ade8ec160bae416935ef34de5f950fea8f8892e2
Jędrzej Kalinowski <kalinet@poczta.onet.pl>
2013-12-15 13:42:01 +0100

    logowanie i liczba zdjęć w albumie

--- dogpics/views.py
+++ dogpics/views.py
@@ -1,6 +1,7 @@
 import logging
 
 #from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse, HttpResponseNotFound
 from django.template import Context, loader
 
@@ -20,11 +21,13 @@ def albums_view(request):
 def album_view(request, album_id=None):
     if album_id is None:
         return HttpResponseNotFound('<h1>Album not found</h1>')
-
-    album = Album.objects.get(pk=album_id)
-    pictures = Picture.objects.filter(album=album)
-    logger.info('Loaded pictures in album '+album.name+',', len(pictures))
-
+    try:
+        album = Album.objects.get(pk=album_id)
+        pictures = Picture.objects.filter(album=album)
+        logger.info('Loaded pictures in album '+album.name+',', len(pictures))
+    except ObjectDoesNotExist:
+        logger.exception('Exception when retrieving album with id ', album_id)
+        return HttpResponseNotFound('<h1>Album '+unicode(album_id)+' not found</h1>')
     t = loader.get_template('dogpics/album.html')
     album_header = 'Album '+album.name+' by '+album.author.name
     c = Context({'album': album, 'album_header': album_header, 'pictures': pictures})
